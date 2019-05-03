13da2a211d5fa9b11d1ae0ded08148afed76f2be
Hermann Krohn <hermannhumberto@gmail.com>
2019-05-03 16:57:13 -0400

    Nearly done login portal

---
+++ routes/users.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var router = express.Router();
+var passport =require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var User = require('../models/user');
+
+//Register
+router.get('/register', function(req, res){
+	res.render('register');
+});
+
+//Login
+router.get('/login', function(req, res){
+	res.render('login');
+});
+
+//Register User
+router.post('/register', function(req, res){
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	//Validation
+	req.checkBody('name', 'Name is Required').notEmpty();
+	req.checkBody('username', 'Username is Required').notEmpty();
+	req.checkBody('email', 'Email is Required').notEmpty();
+	req.checkBody('email', 'Email not valid').isEmail();
+	req.checkBody('password', 'Password is Required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(password);
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('register',{
+			e: errors
+		});
+	}else{
+		var newUser = new User({
+			name: name,
+			email: email,
+			username: username,
+			password: password
+		});
+		User.createUser(newUser, function(err, user){
+			if(err){
+				throw err;
+			}
+			console.log(user);
+		});
+		req.flash('success_msg', 'You are registered and can now login!');
+		res.redirect('login');
+	}
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  User.getUserByUsername(username, function(err, user){
+		if(err) throw err;
+		if(!user){
+			return done(null, false, {message: 'Incorrect Username or Password'})
+		}
+
+		User.comparePassword(password, user.password, function(err, isMatch){
+			if(err) throw err;
+			if(isMatch){
+				return done(null, user)
+			}else{
+				return done(null, false,  {message: 'Incorrect Username or Password'})
+			}
+		});
+	});
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+router.post('/login',
+  passport.authenticate('local', {successRedirect:'/', failureRedirect:'/users/login', failureFlash: true}),
+  function(req, res) {
+		res.redirect('/');
+  });
+
+router.get('/logout', function(req, res){
+	req.logout();
+	req.flash('success_msg', 'You have been logged out');
+	res.redirect('login');
+});
+
+module.exports = router;
---
+++ test.js
@@ -0,0 +1,6 @@
+
+var name = 'HERMANN';
+
+var url = ('www.skdjfksd' + name);
+
+console.log(url);
